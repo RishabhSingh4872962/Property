656ab06bec417355c8c715c3db9e0758b3b7a6f7
Rishabh Singh <rs4872962@gmail.com>
2025-06-16 18:08:22 +0530

    add a bookings screen for bookings

--- app/(tabs)/Bookings.tsx
+++ app/(tabs)/Bookings.tsx
@@ -1,31 +1,46 @@
-import { StyleSheet } from 'react-native';
+import { FlatList } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+import { Text, View } from "@/components/Themed";
+import { getBookings } from "@/hooks/useBookings";
+import { getProperties } from "@/hooks/getProperties";
+import { useAppStore } from "@/stores/useAppStore";
+import { SafeView } from "@/components/StyledComponents/StyledView";
+import BookingList from "@/components/Bookings/bookingList";
 
 export default function Bookings() {
+  const userId = useAppStore((s) => s.user?.id ?? null);
+
+  const { data: bookings = [], isLoading, error } = getBookings(userId);
+  const { data: properties = [] } = getProperties();
+
+  if (isLoading) {
+    return (
+      <SafeAreaView className="flex-1 items-center justify-center bg-gray-50 px-4">
+        <Text className="text-center text-base text-gray-500">Loading...</Text>
+      </SafeAreaView>
+    );
+  }
+
+  if (error) {
+    return (
+      <SafeAreaView className="flex-1 items-center justify-center bg-gray-50 px-4">
+        <Text className="text-red-500 text-center">
+          Failed to load bookings
+        </Text>
+      </SafeAreaView>
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Bookings</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/two.tsx" />
-    </View>
+    <SafeView>
+      <View className="mb-4 mx-1 bg-white rounded-xl shadow-sm border border-gray-100 p-4">
+        <Text className="text-xl font-bold text-black text-center">
+          My Bookings
+        </Text>
+      </View>
+
+      <BookingList properties={properties} bookings={bookings} />
+    </SafeView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
