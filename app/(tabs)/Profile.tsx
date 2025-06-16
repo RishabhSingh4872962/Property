8f0fdf92e5490f6d1c625d5d2100d3fe5768d785
Rishabh Singh <rs4872962@gmail.com>
2025-06-16 18:08:00 +0530

    add a profile screen for user

--- app/(tabs)/Profile.tsx
+++ app/(tabs)/Profile.tsx
@@ -1,27 +1,35 @@
-import { StyleSheet } from "react-native";
-import React from "react";
-import { Text, View } from "@/components/Themed";
+import { View, FlatList } from "react-native";
+import { Text } from "@/components/Themed";
+import { useProfile } from "@/hooks/useProfiles";
+import { getBookings } from "@/hooks/useBookings";
+import { getProperties } from "@/hooks/getProperties";
+import { SafeAreaView } from "react-native-safe-area-context";
+import BookingList from "@/components/Bookings/bookingList";
 
 export default function Profile() {
+  const { data: profile, isLoading: loadingProfile } = useProfile();
+  const { data: bookings = [] } = getBookings(profile?.id ?? "");
+  const { data: properties = [] } = getProperties();
+
+  if (loadingProfile) {
+    return <Text className="text-center mt-10">Loading...</Text>;
+  }
+
   return (
-    <View>
-      <Text style={styles.title}>Profile Screen</Text>
-    </View>
+    <SafeAreaView className="flex-1 bg-gray-50">
+      <View className="flex-1 p-4">
+        <Text className="text-2xl font-bold mb-2 text-black">Profile</Text>
+        <Text className="text-base text-gray-700">Name: {profile.name}</Text>
+        <Text className="text-base text-gray-700 mb-4">
+          Email: {profile.email}
+        </Text>
+
+        <Text className="text-xl font-semibold mb-2 text-black">
+          My Bookings
+        </Text>
+
+        <BookingList bookings={bookings} properties={properties} />
+      </View>
+    </SafeAreaView>
   );
 }
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
-  },
-});
